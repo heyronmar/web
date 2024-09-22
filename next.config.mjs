285f669fc491aecfe475a1c4287da62ea9e54096
heyronmar <weroarltd@gmail.com>
2024-09-22 23:06:25 +1200

    change

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
 
     // output: "export", //app router configuration as it hooks in my github account
     // unoptimized: true 
+
+    basePath: '/heyronmar', // Replace with your repo name
+    assetPrefix: '/heyronmar', // Optional but recommended
     
 };
 
 export default nextConfig;
+  
