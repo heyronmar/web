9271f04c669ca1fd9acefa3ee1e296d40dd3addf
heyronmar <weroarltd@gmail.com>
2024-09-22 23:17:47 +1200

    reset to default

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export", // Enables static export for GitHub Pages
-    // images: {
-    //     unoptimized: true, // Disables Next.js image optimization (required for static export)
-    // },
+
+
     
-    basePath: '/heyronmar', // Replace with your GitHub repository name
-    assetPrefix: '/heyronmar/', // Ensure the asset prefix has a trailing slash
 };
 
 export default nextConfig;
