0660972bb1f5800fc695cc5fdf8baa2352c576e9
heyronmar <weroarltd@gmail.com>
2024-09-22 23:13:17 +1200

    next new

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", // Enables static export for GitHub Pages
+    // output: "export", // Enables static export for GitHub Pages
     images: {
         unoptimized: true, // Disables Next.js image optimization (required for static export)
     },
