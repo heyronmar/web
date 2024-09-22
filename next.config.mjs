0172e9499f26466d0a42b9739714b1bed32cf194
heyronmar <weroarltd@gmail.com>
2024-09-22 23:15:22 +1200

    remove config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: "export", // Enables static export for GitHub Pages
-    images: {
-        unoptimized: true, // Disables Next.js image optimization (required for static export)
-    },
+    // images: {
+    //     unoptimized: true, // Disables Next.js image optimization (required for static export)
+    // },
     
     basePath: '/heyronmar', // Replace with your GitHub repository name
     assetPrefix: '/heyronmar/', // Ensure the asset prefix has a trailing slash
