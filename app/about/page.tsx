b3efc1872371c5a787ad1b2aeec6aaed2d403c7f
heyronmar <weroarltd@gmail.com>
2024-09-24 23:36:49 +1200

    cv included

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,19 +12,8 @@ const AboutPage = () => {
         <div className='flex flex-col'>
           <div className='xl:max-w-[80%] py-8'>
             <h1 className='text-3xl font-extralight text-zinc-600 leading-snug tracking-wide pb-6'>
-              <span className='text-zinc-700'>I’m a digital designer obsessed with human-computer interaction</span>. I have a wide experience in <span className='underline decoration-blue-300 '>UX</span>, <span className='underline decoration-blue-300 '>mobile app & web design</span>, <span className='underline decoration-blue-300 '>design systems</span>, <span className='underline decoration-blue-300 '>research & testing</span>, <span className='underline decoration-blue-300 '>product strategy</span>, <span className='underline decoration-blue-300 '>product management</span>, <span className='underline decoration-blue-300 '>design and engineering</span>. <span className='text-zinc-700'>My goal is to design solutions that bring <span className='bg-teal-200'>delightful experiences to users</span> while meeting <span className='bg-teal-200'>business requirements</span>.</span>
+              <span className='text-zinc-700'>I’m a digital designer obsessed with human-computer interaction</span>. I have a wide experience in <span className='underline decoration-blue-300 '>UX</span>, <span className='underline decoration-blue-300 '>mobile app & web design</span>, <span className='underline decoration-blue-300 '>design systems</span>, <span className='underline decoration-blue-300 '>research & testing</span>, <span className='underline decoration-blue-300 '>product strategy</span>, <span className='underline decoration-blue-300 '>product management</span>, <span className='underline decoration-blue-300 '>design and engineering</span>. <span className='text-zinc-700'>My goal is to design solutions that bring <span className='bg-teal-200'>delightful experiences to users</span> while meeting <span className='bg-teal-200'>business requirements</span>. </span><Link href={"./assets/2024 CV Ronmar Lacamiento-min.pdf"} className="text-zinc-600 hover:text-blue-600" target="_blank">You can download my cv here</Link>
             </h1>
-            {/* <div>
-              <Link href={""} className="text-zinc-600 hover:text-blue-600 text-lg flex items-center gap-2 py-8" target="_blank">
-                  <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
-                    <path fillRule="evenodd" d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.379a.75.75 0 0 1-.53-.22L11.47 3.66A2.25 2.25 0 0 0 9.879 3H4.5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h15Zm-6.75-10.5a.75.75 0 0 0-1.5 0v4.19l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V10.5Z" clipRule="evenodd" />
-                  </svg>
-
-                  </div>
-                  <div className="text-xl font-light text-zinc-600  hover:text-blue-600 leading-snug tracking-normal">Download a copy of my cv here</div>
-                </Link>
-            </div> */}
           </div>
           
           <div className='py-2'>
